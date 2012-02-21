b9d7acf52049e3fb9c3387acd8096c9d7c215617
Matt Sweeney <mattswe@gmail.com>
2012-02-21 10:51:40 -0800

    Plot two traces

--- dateexp.py
+++ dateexp.py
@@ -1,28 +1,30 @@
-"""
-When plotting time series, eg financial time series, one often wants
-to leave out days on which there is no data, eh weekends.  The example
-below shows how to use an 'index formatter' to achieve the desired plot
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import matplotlib.cbook as cbook
 
-#datafile = cbook.get_sample_data('aapl.csv', asfileobj=False)
-datafile = '/Users/matt/Development/weathermodel/724940TY.csv'
+import os
+
+datafile = cbook.get_sample_data('aapl.csv', asfileobj=False)
 print 'loading', datafile
-r = mlab.csv2rec(datafile, skiprows=1)
+r1 = mlab.csv2rec(datafile)
 
-r.sort()
-r = r[-300:]
+datafile = os.getcwd()+'/724940TY.csv'
+print 'loading', datafile
+r2 = mlab.csv2rec(datafile, skiprows=1)
 
-print r.dtype.names
+r1.sort()
+r1 = r1[-300:]
+
+#print r1.dtype.names
 
 fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(r.date_mmddyyyy, r.dewpoint_c, 'o-')
-#ax.plot(r.date, r.open, 'o-')
+
+ax1 = fig.add_subplot(211)
+ax1.plot(r1.date, r1.open, 'o-')
+
+ax2 = fig.add_subplot(212)
+ax2.plot(r2.date_mmddyyyy, r2.dewpoint_c, 'o-')
+
 fig.autofmt_xdate()
-
-
 plt.show()
