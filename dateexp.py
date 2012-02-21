571f37ac4c2ff701c8564d2adcf60c3bdf0e65cb
Matt Sweeney <mattswe@gmail.com>
2012-02-21 11:06:05 -0800

    Date, time combination

--- dateexp.py
+++ dateexp.py
@@ -4,6 +4,7 @@ import matplotlib.mlab as mlab
 import matplotlib.cbook as cbook
 
 import os
+import datetime
 
 datafile = cbook.get_sample_data('aapl.csv', asfileobj=False)
 print 'loading', datafile
@@ -13,10 +14,16 @@ datafile = os.getcwd()+'/724940TY.csv'
 print 'loading', datafile
 r2 = mlab.csv2rec(datafile, skiprows=1)
 
-for r in r2:
-	print r
 
-quit()
+# combine date and time columns
+for r in r2:
+    raw_time = r[1].split(':')
+    raw_time = [int(t) for t in raw_time]
+    if raw_time[0] == 24:
+        raw_time[0] = 0
+
+    time = datetime.time(raw_time[0],raw_time[1])
+    datetime.datetime.combine(r[0], time)
 
 
 r1.sort()
