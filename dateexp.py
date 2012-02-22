556b8e2770e89805bafac7260a575bc9ad4b9a21
Matt Sweeney <mattswe@gmail.com>
2012-02-22 12:52:08 -0800

    Using pandas exponetial smoothing

--- dateexp.py
+++ dateexp.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-import datetime
+import pandas
 
 import parsetmy
 import getwunderground
+
 
 r1 = parsetmy.load()
 r2 = getwunderground.load()
 
 fig = plt.figure()
-
-
 ax1 = fig.add_subplot(111)
-ax1.plot(r1.date_mmddyyyy, r1.dewpoint_c, 'o-')
+
+#ax1.plot(r1.date_mmddyyyy, r1.dewpoint_c, 'o-')
+ax1.plot(r1.date_mmddyyyy, pandas.stats.moments.ewma(r1.dewpoint_c,span=100), 'o-')
 ax1.plot(r2['datetime'], r2['dewptm'], 'o-')
 
 #ax2 = fig.add_subplot(212)
