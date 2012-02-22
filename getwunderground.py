574e1de9a3f115f562b7ed12e4fc3db7491fe915
Matt Sweeney <mattswe@gmail.com>
2012-02-21 16:05:52 -0800

    Concat daily wundergound arrays

--- getwunderground.py
+++ getwunderground.py
@@ -3,28 +3,54 @@
 import requests
 import json
 import datetime
-from numpy import array, zeros
+import time
+from numpy import array, zeros, concatenate
 
 def jsondate(date_dict):
+    """transform json date into datetime"""
 
     yr = int(date_dict['year'])
     mn = int(date_dict['mon'])
     dy = int(date_dict['mday'])
     hr = int(date_dict['hour'])
+    mi = int(date_dict['min'])
 
-    return datetime.datetime(yr,mn,dy,hr)
+    return datetime.datetime(yr,mn,dy,hr,mi)
 
+def fetchday(date):
+    "Get observations for one day, put into record"
 
-api_key = 'fae7d20f3531b884'
-date = '20101018'
-url = 'http://api.wunderground.com/api/%s/history_%s/q/SFO.json' % (api_key, date)
-r = requests.get(url)
+    # fetch that thang
+    api_key = 'fae7d20f3531b884'
+    url = 'http://api.wunderground.com/api/%s/history_%s/q/SFO.json' % (api_key, date)
+    r = requests.get(url)
+    entry = json.loads(r.content)
+    print 'fetched ', date
+    
+    # rate limit
+    print 'sleeping..'
+    time.sleep(10)
 
-entry = json.loads(r.content)
+    # find out how many observations occured (not always 24)
+    num_of_entries = len(entry['history']['observations'])
 
-x = zeros(25, dtype=[('datetime', '|O8'), ('dewptm', '<f8')])
+    x = zeros(num_of_entries, dtype=[('datetime', '|O8'), ('dewptm', '<f8')])
 
-for index, day in enumerate(entry['history']['observations']):
-    x[index] = (jsondate(day['date']), float(day['dewptm']))
+    for index, day in enumerate(entry['history']['observations']):
+        x[index] = (jsondate(day['date']), float(day['dewptm']))
 
-print x
+    return x
+
+def getrange(date, num):
+
+    daily_arrays = []
+
+    for day in range(num):
+
+        string_date = date.strftime('%Y%m%d')
+        daily_arrays.append(fetchday(string_date))
+        date += datetime.timedelta(1)
+
+    return concatenate(daily_arrays, axis=1)
+
+print getrange(datetime.datetime(2011,8,5), 5)
